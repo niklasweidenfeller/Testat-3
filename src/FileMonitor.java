ab75ae39edbdf35dd4413f455aa68dffd0532e03
Niklas Weidenfeller <niklas.weidenfeller@sap.com>
2022-01-25 20:07:14 +0100

    refactor FileMonitor

--- src/FileMonitor.java
+++ src/FileMonitor.java
@@ -1,31 +1,28 @@
 public class FileMonitor {
-    boolean activeReader = false;
     boolean activeWriter = false;
     int readCount = 0;
-    int waitingWriters = 0;
+    int writeCount = 0;
 
     public synchronized void startRead() {
         try {
-            while (activeWriter || waitingWriters>0) wait();
+            while (writeCount>0) wait();
             readCount++;
-            if (readCount == 1) activeReader = true;
             notifyAll();
         } catch (InterruptedException e) {}
     }
     public synchronized void endRead() {
         readCount--;
-        if (readCount == 0) activeReader = false;
         notifyAll();
     }
     public synchronized void startWrite() {
         try {
-            waitingWriters++;
-            while (activeReader || activeWriter) wait();
+            writeCount++;
+            while (readCount>0 || activeWriter) wait();
             activeWriter = true;
-            waitingWriters--;
         } catch (InterruptedException e) {}
     }
     public synchronized void endWrite() {
+        writeCount--;
         activeWriter = false;
         notifyAll();
     }
